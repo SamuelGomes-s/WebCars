2a4319f9555839dee7c0243b0e834d64eacd3277
Samuel Gomes <sam2022gs@gmail.com>
2025-01-07 19:00:50 -0300

    Ajustes Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,21 +8,27 @@ import { IoMdLogIn } from "react-icons/io";
 import { IoPersonCircleOutline } from "react-icons/io5";
 
 export default function Header() {
-    
+
     const loadingAuth = false;
     const signed = false
-    
+
     return (
         <HeaderContainer>
             <HeaderContent>
                 <Logo />
                 {signed && !loadingAuth && (
                     <ActionBtn to={'/dashboard'} >
-                        <IoPersonCircleOutline color={"#000"} size={45} />
-                    </ActionBtn>)}
-                {!signed && !loadingAuth && (<ActionBtn to={'/login'} >
-                    <IoMdLogIn color="#000" size={45} />
-                </ActionBtn>)}
+                        <IoMdLogIn color="#000" size={45} />
+                    </ActionBtn>
+                )
+                }
+                {
+                    !signed && !loadingAuth && (
+                        <ActionBtn to={'/login'} >
+                            <IoPersonCircleOutline color={"#000"} size={45} />
+                        </ActionBtn>
+                    )
+                }
             </HeaderContent>
         </HeaderContainer>
     )
